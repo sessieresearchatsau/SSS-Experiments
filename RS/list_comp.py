c58b3391402ceb7e46a9ff165c3126196d984f0b
Rhys Sharpe <davidrsharpe@southern.edu>
2024-12-13 10:21:56 -0500

    Implemented dec_pattern_1 in Python

--- RS/list_comp.py
+++ RS/list_comp.py
@@ -4,5 +4,9 @@ def test_list_comps(n: int) -> None:
         # 9-0 sequence for this same representation
         print([j for j in range(n, -1, -i)])
         
+def dec_pattern_1(n: int) -> None:
+    print([[n-j-i for j in range(0, n-i)] for i in range(0, n)])
+    
 if __name__ == "__main__":
     test_list_comps(10)
+    dec_pattern_1(3)
