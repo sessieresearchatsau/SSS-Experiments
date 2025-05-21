2c3f82676082faeeceb7e570858146c13c0d19c0
Rhys Sharpe <118227328+Cyneric400@users.noreply.github.com>
2025-05-21 11:02:01 -0400

    Tweaking efficiency of cpp snippet

--- RS/string_concat.cpp
+++ RS/string_concat.cpp
@@ -1,15 +1,15 @@
 // Generates the string from Equation 14
 // https://www.geeksforgeeks.org/how-to-convert-a-single-character-to-string-in-cpp/
-
+// Help from Aiden Gariepy on efficiency tweaks
 
 #include <iostream>
 
 int main() {
-    std::string str = "";
+    std::string str;
     for (int i = 0; i <= 3; i++) {
         for (int j = 0; j <= 2; j++) {
             char new_char = 'A' + i + j;
-            str = str + new_char;
+            str.push_back(new_char);
         }
     }
     std::cout << str << std::endl;
